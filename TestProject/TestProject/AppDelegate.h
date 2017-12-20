197f2d84688b2a4805031ab0fda29fd155fded65
gongxiaobo <gongxiaobo@didichuxing.com>
2017-12-20 15:15:07 +0800

    commit testproject origin

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by didi on 2017/12/19.
+//  Copyright © 2017年 didi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
